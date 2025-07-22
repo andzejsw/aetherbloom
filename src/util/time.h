0b5b14dbd86478869e5bb22f36adb65029a2cb35
Andris Balodis <andris.balodis@enreach.com>
2025-07-22 17:19:13 +0300

    time.h contents more readable

--- src/util/time.h
+++ src/util/time.h
@@ -2,26 +2,32 @@
 #define TIME_H
 
 #ifdef _WIN32
-#include <windows.h>
+    #include <windows.h>
 
-static LARGE_INTEGER _frequency;
-static bool _frequency_initialized = false;
+    static LARGE_INTEGER _frequency;
+    static bool _frequency_initialized = false;
 
-#define NS_PER_SECOND (1000000000ULL)
-#define NS_PER_MS (1000000ULL)
+    #define NS_PER_SECOND (1000000000ULL)
+    #define NS_PER_MS (1000000ULL)
 
-#define NOW() ({ if (!_frequency_initialized) { QueryPerformanceFrequency(&_frequency); _frequency_initialized = true; } LARGE_INTEGER current_time; QueryPerformanceCounter(&current_time); (u64)((double)current_time.QuadPart / _frequency.QuadPart * NS_PER_SECOND); })
-
+    #define NOW() ({ 
+        if (!_frequency_initialized) { 
+            QueryPerformanceFrequency(&_frequency); 
+            _frequency_initialized = true; 
+        } 
+        LARGE_INTEGER current_time; 
+        QueryPerformanceCounter(&current_time); 
+        (u64)((double)current_time.QuadPart / _frequency.QuadPart * NS_PER_SECOND); 
+    })
 #else // Linux/Unix
-#include <time.h>
+    #include <time.h>
 
-#define NS_PER_SECOND (1000000000)
-#define NS_PER_MS (1000000)
+    #define NS_PER_SECOND (1000000000)
+    #define NS_PER_MS (1000000)
 
-#define NOW() ({\
-    struct timespec ts;\
-    timespec_get(&ts, TIME_UTC);\
-    ((ts.tv_sec * NS_PER_SECOND) + ts.tv_nsec);})
-#endif
-
+    #define NOW() ({\
+        struct timespec ts;\
+        timespec_get(&ts, TIME_UTC);\
+        ((ts.tv_sec * NS_PER_SECOND) + ts.tv_nsec);})
+    #endif
 #endif
